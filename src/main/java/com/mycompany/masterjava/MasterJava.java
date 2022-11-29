d814a01e549d208030424c43cdc79c72afe54b18
Ezra <kimatizyler@gmail.com>
2022-11-29 03:43:09 +0000

    after creating an action chooser

--- src/main/java/com/mycompany/masterjava/MasterJava.java
+++ src/main/java/com/mycompany/masterjava/MasterJava.java
@@ -5,13 +5,39 @@
 
 package com.mycompany.masterjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author KamsAdmin
  */
 public class MasterJava {
-
+    static MasterJava masterjava = new MasterJava();
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("---------------Choose Test Drive--------------\n 1. Print Hello Ezra \t\t 2. Test");
+        Scanner action = new Scanner(System.in);
+        int choice = action.nextInt();
+        
+        //switch choice
+        switch(choice){
+            case 1:
+                 System.out.println("Hello Ezra");
+                 masterjava.ezraResponse();
+                break;
+            
+            case 2:
+                
+                break;
+                
+            default:
+                System.out.println("Kimati is asking you whether you know what you are doing or not!!!!");
+                break;
+        }
+        
+    }
+    
+    public void ezraResponse(){
+        System.out.println("Hello, this is Ezra Kimati. Responding to you from the Plains [Hahahaha]. How may i help?");
     }
 }
